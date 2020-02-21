20adc453d144945146dddf4cc269c55177740569
goliverma <gcool73@gmail.com>
2020-02-21 22:15:56 +0530

    this change for locate default controller.

--- employee/Startup.cs
+++ employee/Startup.cs
@@ -16,6 +16,7 @@ namespace employee
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,10 @@ namespace employee
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{Action=Index}/{id?}"
+                );
             });
         }
     }
