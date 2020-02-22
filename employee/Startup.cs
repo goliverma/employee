960568cc2149ba3245d1f015ec19365bb41f08e7
goliverma <gcool73@gmail.com>
2020-02-22 20:20:54 +0530

    normal change.

--- employee/Startup.cs
+++ employee/Startup.cs
@@ -21,7 +21,6 @@ namespace employee
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
