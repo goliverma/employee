17557133268dd7eed414fb8c23eec5d5ce72a065
goliverma <gcool73@gmail.com>
2020-02-22 21:04:17 +0530

    this change for using dependency injection for .net core framework.

--- employee/Startup.cs
+++ employee/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using employee.Models.Interfaces;
+using employee.Models.Mock;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +23,7 @@ namespace employee
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
