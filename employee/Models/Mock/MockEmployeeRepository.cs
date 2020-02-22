02bb3e66bdbae1199671d0a543704212038e4bf0
goliverma <gcool73@gmail.com>
2020-02-22 20:52:26 +0530

    we make mockemployeerepository for store hardcoded data this data is for retriving data from controller.

---
+++ employee/Models/Mock/MockEmployeeRepository.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using employee.Models.Data;
+using employee.Models.Interfaces;
+
+namespace employee.Models.Mock
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> employeelist;
+        public MockEmployeeRepository()
+        {
+            employeelist = new List<Employee>(){
+                new Employee(){Id=1, Name="Gaurav Verma", Department="IT", Email="gaurav@gmail.com"},
+                new Employee(){Id=2, Name="Gopal", Department="IT", Email="gopal@gmail.com"},
+                new Employee(){Id=3, Name="Gagan", Department="HR", Email="gagan@gmail.com"},
+                new Employee(){Id=4, Name="Yashu", Department="Payrol", Email="yashu@gmail.com"}
+            };
+        }
+        public Employee GetEmployee(int id) => employeelist.FirstOrDefault(e => e.Id == id);
+    }
+}
