cdfe61309dabea6f942b35217990559b834ace4f
goliverma <gcool73@gmail.com>
2020-02-22 20:59:06 +0530

    i'am use interface with dependency injection in home controller.

--- employee/Controllers/HomeController.cs
+++ employee/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
+using employee.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace employee.Controllers
 {
     public class HomeController : Controller
     {
+        private IEmployeeRepository employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
         public string Index()
         {
             string st="gaurav";
