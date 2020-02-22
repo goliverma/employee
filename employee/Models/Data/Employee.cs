3fb213c5ca309ccc191197469ea9fbbdc639e225
goliverma <gcool73@gmail.com>
2020-02-22 20:33:31 +0530

    we make employee.cs model Data Directory.

---
+++ employee/Models/Data/Employee.cs
@@ -0,0 +1,10 @@
+namespace employee.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+    }
+}
