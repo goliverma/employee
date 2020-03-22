a4e293c8afd6ca1b71533f23f4bebb9abaa472ae
goliverma <gcool73@gmail.com>
2020-03-22 13:26:03 +0530

    i'm include a style sheet.

--- employee/Models/Mock/MockEmployeeRepository.cs
+++ employee/Models/Mock/MockEmployeeRepository.cs
@@ -11,10 +11,10 @@ namespace employee.Models.Mock
         public MockEmployeeRepository()
         {
             employeelist = new List<Employee>(){
-                new Employee(){Id=1, Name="Gaurav", Department="IT", Email="gaurav@gmail.com"},
-                new Employee(){Id=2, Name="Gopal", Department="IT", Email="gopal@gmail.com"},
-                new Employee(){Id=3, Name="Gagan", Department="HR", Email="gagan@gmail.com"},
-                new Employee(){Id=4, Name="Yashu", Department="Payrol", Email="yashu@gmail.com"}
+                new Employee(){Id=0, Name="Gaurav", Department="IT", Email="gaurav@gmail.com"},
+                new Employee(){Id=1, Name="Gopal", Department="IT", Email="gopal@gmail.com"},
+                new Employee(){Id=2, Name="Gagan", Department="HR", Email="gagan@gmail.com"},
+                new Employee(){Id=3, Name="Yashu", Department="Payrol", Email="yashu@gmail.com"}
             };
         }
 
