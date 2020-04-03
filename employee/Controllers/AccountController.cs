fd224c97edeb8ba0c861767fd4574ca4282862cf
goliverma <gcool73@gmail.com>
2020-04-03 22:33:47 +0530

    i'm add user register page in my project

---
+++ employee/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+using System.Threading.Tasks;
+using employee.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employee.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager=userManager;
+            this.signInManager=signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user=new IdentityUser{UserName=model.Email, Email=model.Email};
+                var result= await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index","Home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ employee/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace employee.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name="Confirm Password")]
+        [Compare("Password", ErrorMessage="Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
